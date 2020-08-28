258aaa75476973566618af6a86fa3f321522029a
Satit Porntepanon <st_satitza@hotmail.com>
2020-08-29 02:44:14 +0700

    initial project

---
+++ src/main/java/com/anonymous/spring/system/ComAnonymousSpringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.anonymous.spring.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComAnonymousSpringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComAnonymousSpringSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anonymous/spring/system/ComAnonymousSpringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anonymous.spring.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComAnonymousSpringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

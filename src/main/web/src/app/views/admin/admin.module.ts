71a859eed2bf1905f2b63a7d5a2185fd56e7feb3
Satit Porntepanon <st_satitza@hotmail.com>
2020-09-07 20:06:30 +0700

    commit -*-

--- src/main/web/src/app/views/admin/admin.module.ts
+++ src/main/web/src/app/views/admin/admin.module.ts
@@ -1,5 +1,16 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AdminRoutingModule} from "./admin.routing";
+import {LogHistoryComponent} from './log-history/log-history.component';
 
 
+@NgModule({
+  declarations: [LogHistoryComponent],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
 export class AdminModule {
 
 }

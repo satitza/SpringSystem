6b8b2fbb8b0c4f85670cabc8a6c60b0029c86d49
Satit Porntepanon <st_satitza@hotmail.com>
2020-09-07 14:06:21 +0700

    add admin module

---
+++ src/main/web/src/app/views/admin/admin.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AdminRoutingModule} from "./admin.routing";
+import {LogHistoryComponent} from './log-history/log-history.component';
+
+
+@NgModule({
+  declarations: [LogHistoryComponent],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule {
+}
